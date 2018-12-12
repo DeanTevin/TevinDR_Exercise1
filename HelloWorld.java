424286762cc21501260c96c9fc8ee2fe3a58eae8
DeanTevin <Deantevinn@gmail.com>
2018-12-12 14:57:58 +0700

    Unstaged File will be added as staged

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class Sample
 		public static void main(String args[]){
 			System.out.println("This is sample for git in java programming")
 			System.out.println("Testing for adding changes to see the output")
+			System.out.println("Staged and Unstaged File")
 		}
 	}
