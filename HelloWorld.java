7f2088667533224ac54872f5b1a033981baaee61
DeanTevin <Deantevinn@gmail.com>
2018-12-12 15:13:41 +0700

    Task2 UrlChecker

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 public class Sample 
 	{
 		public static void main(String args[]){
-			System.out.println("This is sample for git in java programming")
-			System.out.println("Testing for adding changes to see the output")
-			System.out.println("Staged and Unstaged File")
+			System.out.println("This is sample for git in java programming");
+			System.out.println("Testing for adding changes to see the output");
+			System.out.println("Staged and Unstaged File");
 		}
 	}
